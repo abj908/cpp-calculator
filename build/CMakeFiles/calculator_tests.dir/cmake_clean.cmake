f5edd8d638f2596ca2b90fbb616b389b32f695e7
Albert Joseph <70643134+abj908@users.noreply.github.com>
2024-12-29 22:10:07 +0000

    inital commit with tests and build working

---
+++ build/CMakeFiles/calculator_tests.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/calculator_tests.dir/src/calculator.cpp.o"
+  "CMakeFiles/calculator_tests.dir/tests/calculator_tests.cpp.o"
+  "calculator_tests"
+  "calculator_tests.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/calculator_tests.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
