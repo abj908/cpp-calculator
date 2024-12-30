d18f01ab638315022ba4ac7538ee8f5361683a17
Albert Joseph <70643134+abj908@users.noreply.github.com>
2024-12-30 00:46:57 +0000

    ok

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,7 @@
 #include "calculator.h"
 
 int main() {
-    std::cout << "Hello, Debugging!" << std::endl; // Breakpoint here
+    //std::cout << "Hello, Debugging!" << std::endl; // Breakpoint here
     int a, b;
     char op;
 
